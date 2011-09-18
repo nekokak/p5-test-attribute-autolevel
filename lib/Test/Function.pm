ad1188001728f2dc895cd9c1b02bbf006b5f6420
Atsushi Kobayashi <nekokak@gmail.com>
2011-09-18 23:02:27 +0900

    check attribute name

--- lib/Test/Function.pm
+++ lib/Test/Function.pm
@@ -7,7 +7,7 @@ sub import {
     my $caller = caller(0);
 
     no strict 'refs';
-    *{"${caller}::MODIFY_CODE_ATTRIBUTES"} = \&MODIFY_CODE_ATTRIBUTES;
+    *{"${caller}::MODIFY_CODE_ATTRIBUTES"} = \&_MODIFY_CODE_ATTRIBUTES;
 }
 
 sub _fake {
@@ -25,8 +25,9 @@ sub _fake {
     *{"${pkg}::${funcname}"} = $fake;
 }
 
-sub MODIFY_CODE_ATTRIBUTES {
+sub _MODIFY_CODE_ATTRIBUTES {
     my ($pkg, $code, @attrs) = @_;
+    return unless $attrs[0] =~ /^Test$/;
     _fake($pkg, $code);
     return;
 }
@@ -37,21 +38,29 @@ __END__
 
 =head1 NAME
 
-Test::Function -
+Test::Function - auto set $Test::Builder::Level
 
 =head1 SYNOPSIS
 
+  use Test::More;
   use Test::Function;
+  
+  sub test_foo : Test {
+      ok 0;
+  }
+  test_foo(); # test failed. report line at call test_foo().
 
 =head1 DESCRIPTION
 
-Test::Function is
+Test::Function is auto set $Test::Builder::Level.
 
 =head1 AUTHOR
 
 Atsushi Kobayashi E<lt>nekokak _at_ gmail _dot_ comE<gt>
 
-=head1 SEE ALSO
+=head1 CONTRIBUTORS
+
+kamipo
 
 =head1 LICENSE
 
--- t/hoge.t
+++ t/hoge.t
@@ -3,29 +3,34 @@ use warnings;
 use Test::More;
 use Test::Function;
 
-sub test_foo : test_foo {
+sub test_moge : TEST {
     ok 0;
 }
 
-sub test_nest2 : test_nest2 {
+sub test_foo : Test {
     ok 0;
 }
 
-sub test_nest1 : test_nest1 {
+sub test_nest2 : Test {
+    ok 0;
+}
+
+sub test_nest1 : Test {
     ok 0;
     test_nest2();
 }
 
-sub test_nest3 : test {
+sub test_nest3 : Test {
     ok 0;
     test_nest2();
 }
 
-sub test_nest4 : test {
+sub test_nest4 : Test {
     ok 0;
     test_nest1();
 }
 
+test_moge();
 test_foo();
 test_nest1();
 test_nest3();
